4a5a69a0d450a288749167be933b7e895fdd7e13
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-06-19 11:39:43 +0300

    ver 0.65 Implemented 2nd upgrade/Added visible CF and CPS

--- clicker-game-3/src/ClickerScreen.jsx
+++ clicker-game-3/src/ClickerScreen.jsx
@@ -9,104 +9,128 @@ function ClickerScreen({setScreen}) {
     
     const [totalClicks, setTotalCicks] = useState(0);
     const [clickForce, setClickForce] = useState(1);
-    const [cps, setCps] = useState(1);
+    const [cps, setCps] = useState(0);
     const [achievementTotal, setAchievementTotal] = useState(0);
     const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
     const [popUpVisibility,setPopUpVisibility] = useState(false);
     const [popUpText, setPopUpText] = useState("");
     const [section, setSection] = useState("Upgrades");
-    const [dabloons, setDabaloons] = useState(0);
+    const [dabloons, setDabloons] = useState(0);
     const [increaseAmount, setIncreaseAmount] = useState(1);
     const [upgrade1Cost, setUpgrade1Cost] = useState(30);
+    const [upgrade2Cost, setUpgrade2Cost] = useState(30);
+    const [upgrade3Cost, setUpgrade3Cost] = useState(30);
+    const [upgrade4Cost, setUpgrade4Cost] = useState(30);
+    const [upgrade1Qty, setUpgrade1Qty] = useState(0);
+    const [upgrade2Qty, setUpgrade2Qty] = useState(0);
+    const [upgrade3Qty, setUpgrade3Qty] = useState(0);
+    const [upgrade4Qty, setUpgrade4Qty] = useState(0);
 
     const backToMenu = () => {
-        setScreen("MainMenu")
+    setScreen("MainMenu")
     }
 
     const handleMouseMove = (event) => {
     // const rect = event.target.getBoundingClientRect()
-        setCursorPosition({
-            x: event.clientX,
-            y: event.clientY
-        });
+    setCursorPosition({
+        x: event.clientX,
+        y: event.clientY
+    });
     };
 
     const displayPopUp = (popUpText) => {
-       setPopUpVisibility(true);
-       setPopUpText(popUpText);
+    setPopUpVisibility(true);
+    setPopUpText(popUpText);
     };
 
     const handleUpgradeSectionChange = () => {
-        setSection("Upgrades");
+    setSection("Upgrades");
     };
     const handleStatsSectionChange = () => {
-        setSection("Stats");
+    setSection("Stats");
     };
     const handleAchievementsSectionChange = () => {
-        setSection("Achievements");
+    setSection("Achievements");
     };
 
     const handleClick = () => {
-        setDabaloons(dabloons + clickForce);
-        setTotalCicks(totalClicks + 1);
+    setDabloons(dabloons + clickForce);
+    setTotalCicks(totalClicks + 1);
     };
 
     return (
-        <div className="clicker-stage" onMouseMove={handleMouseMove}>
-            {popUpVisibility === true && 
-            <div className='popup' style={{position:"absolute",top:cursorPosition.y + "px",left:cursorPosition.x + "px"}}>
-                <p>{popUpText}</p>
-            </div>
-            }
-            {/* <p>x: {cursorPosition.x} y: {cursorPosition.y}</p> */}
-            <button className='menu-btn' onClick={backToMenu}>Back to menu</button>
-            <div className="img-div">
-                <img src={pixminn} alt="Pixmin Img" className='pixmin-img' onClick={handleClick}/>
-                <h1 className='money-text'>{dabloons} Dabloons</h1>
-            </div>
-            <div className='right-div'> 
-                <div className='select-bar'>
-                    <div className='upgrades-img'>
-                        <h2 onClick={handleUpgradeSectionChange}>Upgrades</h2>
-                    </div>
-                    <div className='stats-img'>
-                        <h2 onClick={handleStatsSectionChange}>Stats</h2>
-                    </div>
-                    <div className='achievements-img'>
-                        <h2 onClick={handleAchievementsSectionChange}>Achievements</h2>
-                    </div>
+    <div className="clicker-stage" onMouseMove={handleMouseMove}>
+        {popUpVisibility === true && 
+        <div className='popup' style={{position:"absolute",top:cursorPosition.y + "px",left:cursorPosition.x + "px"}}>
+            <p>{popUpText}</p>
+        </div>
+        }
+        {/* <p>x: {cursorPosition.x} y: {cursorPosition.y}</p> */}
+        <button className='menu-btn' onClick={backToMenu}>Back to menu</button>
+        <div className="img-div">
+            <img src={pixminn} alt="Pixmin Img" className='pixmin-img' onClick={handleClick}/>
+            <h1 className='money-text'>{dabloons} Dabloons</h1>
+            <h5 className='click-force-text'>{clickForce} Click Force</h5>
+            <h5 className='cps-text'>{cps} Clicks per second</h5>
+        </div>
+        <div className='right-div'> 
+            <div className='select-bar'>
+                <div className='upgrades-img'>
+                    <h2 onClick={handleUpgradeSectionChange}>Upgrades</h2>
                 </div>
-                <div className='content-div'>
-                    {section === "Upgrades" && <Upgrades 
-                        dabloons={dabloons} 
-                        setDabaloons={setDabaloons} 
-                        clickForce={clickForce}
-                        setClickForce={setClickForce}
-                        upgrade1Cost={upgrade1Cost}
-                        setUpgrade1Cost={setUpgrade1Cost}
-                        cursorPosition={cursorPosition}
-                        setPopUpVisibility={setPopUpVisibility}
-                        setPopUpText={setPopUpText}
-                        displayPopUp={displayPopUp}
-                        totalClicks={totalClicks}
-                        setTotalCicks={setTotalCicks}
-                        cps={cps}
-                        setCps={setCps}/>}
-                    {section === "Stats" && <Stats
-                        cursorPosition={cursorPosition}
-                        setPopUpVisibility={setPopUpVisibility}
-                        setPopUpText={setPopUpText}
-                        displayPopUp={displayPopUp}
-                        totalClicks={totalClicks}
-                        setTotalCicks={setTotalCicks}
-                        clickForce={clickForce}
-                        setClickForce={setClickForce}
-                        cps={cps}
-                        setCps={setCps}/>}
-                    {section === "Achievements" && <Achievements/>}
+                <div className='stats-img'>
+                    <h2 onClick={handleStatsSectionChange}>Stats</h2>
                 </div>
+                <div className='achievements-img'>
+                    <h2 onClick={handleAchievementsSectionChange}>Achievements</h2>
+                </div>
+            </div>
+            <div className='content-div'>
+                {section === "Upgrades" && <Upgrades 
+                    dabloons={dabloons} 
+                    setDabloons={setDabloons} 
+                    clickForce={clickForce}
+                    setClickForce={setClickForce}
+                    upgrade1Cost={upgrade1Cost}
+                    setUpgrade1Cost={setUpgrade1Cost}
+                    upgrade2Cost={upgrade2Cost}
+                    setUpgrade2Cost={setUpgrade2Cost}
+                    upgrade3Cost={upgrade3Cost}
+                    setUpgrade3Cost={setUpgrade3Cost}
+                    upgrade4Cost={upgrade4Cost}
+                    setUpgrade4Cost={setUpgrade4Cost}
+                    cursorPosition={cursorPosition}
+                    setPopUpVisibility={setPopUpVisibility}
+                    setPopUpText={setPopUpText}
+                    displayPopUp={displayPopUp}
+                    totalClicks={totalClicks}
+                    setTotalCicks={setTotalCicks}
+                    cps={cps}
+                    setCps={setCps}
+                    upgrade1Qty={upgrade1Qty}
+                    setUpgrade1Qty={setUpgrade1Qty}
+                    upgrade2Qty={upgrade2Qty}
+                    setUpgrade2Qty={setUpgrade2Qty}
+                    upgrade3Qty={upgrade3Qty}
+                    setUpgrade3Qty={setUpgrade3Qty}
+                    upgrade4Qty={upgrade4Qty}
+                    setUpgrade4Qty={setUpgrade4Qty}   
+                    />}
+                {section === "Stats" && <Stats
+                    cursorPosition={cursorPosition}
+                    setPopUpVisibility={setPopUpVisibility}
+                    setPopUpText={setPopUpText}
+                    displayPopUp={displayPopUp}
+                    totalClicks={totalClicks}
+                    setTotalCicks={setTotalCicks}
+                    clickForce={clickForce}
+                    setClickForce={setClickForce}
+                    cps={cps}
+                    setCps={setCps}/>}
+                {section === "Achievements" && <Achievements/>}
             </div>
         </div>
+    </div>
     );
 }
 
--- clicker-game-3/src/Upgrades.jsx
+++ clicker-game-3/src/Upgrades.jsx
@@ -1,32 +1,51 @@
 import { useEffect } from "react";
 
-function Upgrades({dabloons,setDabaloons,
-                   upgrade1Cost,clickForce, setClickForce, setUpgrade1Cost,
+function Upgrades({dabloons,setDabloons,
+                   upgrade1Cost, setUpgrade1Cost,upgrade2Cost, setUpgrade2Cost,
+                   upgrade3Cost, setUpgrade3Cost,upgrade4Cost, setUpgrade4Cost,
+                   clickForce, setClickForce,
                    cursorPosition, setPopUpVisibility, setPopUpText,
-                   displayPopUp,cps,setCps}) {
+                   displayPopUp,cps,setCps,upgrade1Qty,setUpgrade1Qty,
+                   upgrade2Qty,setUpgrade2Qty,upgrade3Qty,setUpgrade3Qty,
+                   upgrade4Qty,setUpgrade4Qty,}) {
 
+    //useEffect hook meant for autoClicking function that updates when 
+    //the cps value changes
     useEffect(() => {
-        
         if(cps > 0) {
            const intervalId = setInterval(() => {
-                setDabaloons(prevDabloons => prevDabloons + cps)
+                setDabloons(prevDabloons => prevDabloons + cps)
             }, 1000);
             
            return () => clearInterval(intervalId);
         }
     }, [cps]);
-
+       // function meant for handling the Upgrade 1 Click Force
        const handleUpgrade1 = () => {
-            if(dabloons >= upgrade1Cost) {
-                // alert("You bought upgrade 1!");
+            if(dabloons >= upgrade1Cost && upgrade1Qty <= 10) {
                 setClickForce(clickForce + 1);
-                setDabaloons(dabloons - upgrade1Cost)
-                setUpgrade1Cost(Math.round(upgrade1Cost * 1.5))
+                setDabloons(dabloons - upgrade1Cost);
+                setUpgrade1Cost(Math.round(upgrade1Cost * 1.5));
+                setUpgrade1Qty(prevUpgrade1Qty => prevUpgrade1Qty + 1);
+                // console.log(upgrade1Qty)
+            }else if(upgrade1Qty >= 10){
+                alert("You maxed this upgrade");
             }else {
                 alert("Not enough Dabloons");
             }
         };
-
+       const handleUpgrade2 = () => {
+          if(dabloons >= upgrade2Cost && upgrade2Qty <= 25) {
+            setCps(prevCps => prevCps + 1);
+            setDabloons(prevDabloons => prevDabloons - upgrade2Cost);
+            setUpgrade2Cost(Math.round(upgrade2Cost * 1.5));
+            setUpgrade2Qty(prevUpgrade2Qty => prevUpgrade2Qty + 1);
+          }else if(upgrade2Qty >= 25) {
+            alert("You maxed this upgrade");
+          }else {
+            alert("Not enough Dabloons");
+          }
+        }
         const handleAutoClick = () => {
             console.log(dabloons);
         }
@@ -36,21 +55,25 @@ function Upgrades({dabloons,setDabaloons,
     return (
         <div className='upgrades-div'>
             <div className="upgrade-div">
-                <p>Upgrade 1</p>
+                <p>Upgrade Click Force</p>
                 <button onClick={handleUpgrade1} 
                         onMouseEnter={() => displayPopUp(`Cost:${upgrade1Cost}, +1 Click Force`)}
-                        onMouseLeave={() => setPopUpVisibility(false)}>Buy</button>
+                        onMouseLeave={() => setPopUpVisibility(false)}
+                        id="upgrade1Btn">Buy</button>
             </div>
             <div className="upgrade-div">
-                <p>Upgrade 2</p>
+                <p>Hire Hobos</p>
+                <button onClick={handleUpgrade2} 
+                        onMouseEnter={() => displayPopUp(`Cost:${upgrade2Cost}, +1 CPS`)}
+                        onMouseLeave={() => setPopUpVisibility(false)}
+                        id="upgrade2Btn">Buy</button>
+            </div>
+            <div className="upgrade-div">
+                <p>Hire Low Wage Workers</p>
                 <button>Buy</button>
             </div>
             <div className="upgrade-div">
-                <p>Upgrade 3</p>
-                <button>Buy</button>
-            </div>
-            <div className="upgrade-div">
-                <p>Upgrade 4</p>
+                <p>Hire Salary men</p>
                 <button>Buy</button>
             </div>
         </div>
